c78c931403771f5d7a17d83ed65910b7c42fda61
Miguel Barahona <dnisbarahona007@gmail.com>
2022-05-23 14:34:49 -0600

    Validacion NIT

--- Model/Intermediario.cs
+++ Model/Intermediario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,9 @@ namespace AcsaDemo.Model
         public string nombre { get; set; }
         public string apellido { get; set; }
         public string correo { get; set; }
+
+        [RegularExpression(@"[0-9]{4}[-][0-9]{6}[-][0-9]{3}[-][0-9]{1}",
+         ErrorMessage = "El valor no coincide con el formato de nit [####-#######-###-#]")]
         public string nit { get; set; }
     }
 }
