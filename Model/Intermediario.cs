770a1bd84e15d73cbc0e7630e0f3a56bb7d58758
Miguel Barahona <dnisbarahona007@gmail.com>
2022-05-24 14:05:22 -0600

    Busqueda por Nit funcional, falta limpiar los datos

--- Model/Intermediario.cs
+++ Model/Intermediario.cs
@@ -9,11 +9,18 @@ namespace AcsaDemo.Model
     public class Intermediario
     {
         public string nombre { get; set; }
-        public string apellido { get; set; }
         public string correo { get; set; }
 
         [RegularExpression(@"[0-9]{4}[-][0-9]{6}[-][0-9]{3}[-][0-9]{1}",
          ErrorMessage = "El valor no coincide con el formato de nit [####-#######-###-#]")]
         public string nit { get; set; }
+        public string fecha_nac { get; set; }
+        public string conyuge { get; set; }
+        public string fecha_nac_conyuge { get; set; }
+        public int n_hijos { get; set; }
+        public double suma_titular { get; set; }
+        public double suma_conyuge { get; set; }
+        public double valor_pcuota { get; set; }
+        public double valor_csucesiva { get; set; }
     }
 }
